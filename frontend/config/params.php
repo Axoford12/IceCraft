93858196f3c141bab3a97681f875d5053f2ff2a7
Axoford12 <postmaster@axoford12.cc>
2017-02-17 21:22:28 +0800

    Protect config

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,19 +1,6 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'apiConn' => [
-        // 在此处填写 api url
-        // Enter Your api url here
-        'url' => 'https://www.mcprc.cc/api.php',
 
-        // 在此处填写你的用户名
-        // Enter your user name here
-        'user' => 'Api',
-
-        // 在此处填写你的api key  它会在multicraft中生成
-        // Enter your api key here
-        // It will be generate in your multicraft
-        'key' => 'ASexx-PL2dQBCD'
-    ],
 
 ];
